17cc97809ad1531c5c80537e0f0cc25a2890d096
Elizaveta Kirchanova <58811575+ekirchanova@users.noreply.github.com>
2020-12-07 08:27:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-import StateCalculator as StateCalculator
-import last_var
+import engine.Physics.StateCalculator as StateCalculator
+import gui.last_var as last_var
 import numpy as np
 import pyqtgraph as pg
 import csv
@@ -9,18 +9,20 @@ from PyQt5 import QtCore, QtGui, QtWidgets , Qt
 
 
 class MyWin(QtWidgets.QMainWindow):
-    temperature = 300
+    temperature = 100
     material = "Si"
     count = 100
-    Namin = 1e17
-    Namax = 1e19
-    Nadelta = count
-    Ndmin = 1e17
-    Ndmax = 1e19
-    Nddelta = count
-    Ea = 0.045
-    Ed = 0.045
-    flag = False
+    Namin = 0
+    Namax = 0
+    Ndmin = 0
+    Ndmax = 0
+    Ea = 0
+    Ed = 0
+    Nmin = 0.1
+    Nmax = 1
+    E = 0.045
+    flag_logx = False
+    flag_logy = False
     flag_n = True
     flag_p = False
     index = 3
@@ -30,16 +32,15 @@ class MyWin(QtWidgets.QMainWindow):
         self.calc = StateCalculator.StateCalculator(self.material)
         self.ui = last_var.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.lineEdit_3.textChanged.connect(self.onChanged_Ndmax)
-        self.ui.lineEdit_4.textChanged.connect(self.onChanged_Ed)
-        self.ui.lineEdit_5.textChanged.connect(self.onChanged_Ndmin)
-        #self.ui.lineEdit_6.textChanged.connect(self.onChanged_Ea)
-        #self.ui.lineEdit_7.textChanged.connect(self.onChanged_Namin)
-        #self.ui.lineEdit_8.textChanged.connect(self.onChanged_Namax)
+        self.ui.lineEdit_3.textChanged.connect(self.onChanged_Nmax)
+        self.ui.lineEdit_4.textChanged.connect(self.onChanged_E)
+        self.ui.lineEdit_5.textChanged.connect(self.onChanged_Nmin)
         self.ui.lineEdit_9.textChanged.connect(self.onChanged_Temp)
         self.ui.lineEdit_10.textChanged.connect(self.onChanged_count)
+
         self.ui.pushButton.clicked.connect(self.work_with_button)
-        self.ui.checkBox.stateChanged.connect(self.set_log_axis)
+        self.ui.checkBox.stateChanged.connect(self.set_log_x_axis)
+        self.ui.checkBox_2.stateChanged.connect(self.set_log_y_axis)
         self.ui.checkBox_3.stateChanged.connect(self.set_n)
         self.ui.checkBox_4.stateChanged.connect(self.set_p)
         self.ui.pushButton_2.clicked.connect(self.save_graph)
@@ -57,6 +58,7 @@ class MyWin(QtWidgets.QMainWindow):
     def save_graphs(self):
         self.save_graph("Electron_concentration.csv", self.Electron_concentration)
         self.save_graph("Electron_mobility.csv", self.electron_mobility)
+        self.save_graph("Hole_mobility.csv", self.Hole_mobility)
         self.save_graph("Hole_concentration.csv", self.Hole_concentration)
         self.save_graph("Negative_acceptor_concentration.csv",
                         self.Negative_acceptor_concentration)
@@ -76,18 +78,34 @@ class MyWin(QtWidgets.QMainWindow):
             self.flag_n = False
             self.ui.checkBox_3.setChecked(0)
 
-    def set_log_axis(self):
+    def set_log_x_axis(self):
         if self.ui.checkBox.isChecked():
-            self.flag = True
-        else: self.flag = False
+            self.flag_logx = True
+        else: self.flag_logx = False
+
+    def set_log_y_axis(self):
+        if self.ui.checkBox.isChecked():
+            self.flag_logy = True
+        else: self.flag_logy = False
+
+    def work_with_flag(self):
+        if (self.flag_n):
+            self.Namin = self.Nmin
+            self.Namax = self.Nmax
+            self.Ea = self.E
+        elif(self.flag_p):
+            self.Ndmin = self.Nmin
+            self.Ndmax = self.Nmax
+            self.Ed = self.E
 
     def work_with_button(self):
+        self.work_with_flag()
         self.index = self.ui.tabWidget.currentIndex()
         self.material = self.ui.ComboBox.currentText()
         self.calc.set_material(str(self.material))
         self.calc.set_temperature(float(self.temperature))
-        self.Na = np.linspace(float(self.Namin), float(self.Namax), int(self.count))
-        self.Nd = np.linspace(float(self.Ndmin), float(self.Ndmax), int(self.count))
+        self.Na = np.linspace(float(self.Namin)*1e18, float(self.Namax)*1e18, int(self.count))
+        self.Nd = np.linspace(float(self.Ndmin)*1e18, float(self.Ndmax)*1e18, int(self.count))
         self.calc.set_additions(self.Na, float(self.Ea), self.Nd, float(self.Ed))
         self.calc.update()
         self.full_all_field()
@@ -97,20 +115,26 @@ class MyWin(QtWidgets.QMainWindow):
         pen = pg.mkPen(color=(0, 0, 0), width=1)
         styles = {"color": "#000", "font-size": "20px"}
         elem.setLabel("left", name_graph, **styles)
-        elem.setLabel("bottom", "Acceptor concentration Na (1e18  cmˆ-3)", **styles)
         elem.addLegend()
-        elem.setLogMode(self.flag, False)
-        elem.plot(self.Na , y_values, pen=pen, clear=True)
+        elem.setLogMode(self.flag_logx, self.flag_logy)
+        if(self.flag_n):
+            elem.setLabel("bottom", "Acceptor concentration Na (1e18  cmˆ-3)", **styles)
+            elem.plot(self.Na/1e18 , y_values, pen=pen, clear=True)
+        elif(self.flag_p):
+            elem.setLabel("bottom", "Donor concentration Nd (1e18  cmˆ-3)", **styles)
+            elem.plot(self.Nd/1e18, y_values, pen=pen, clear=True)
 
 
     def draw_graphics(self):
-
         self.draw_graph(self.ui.tab_4, "Electron concentration (cmˆ-3)", self.Electron_concentration)
-        self.draw_graph(self.ui.tab, "Electron mobility (cmˆ2 * Vˆ1 * sˆ-1)", self.electron_mobility)
+        self.draw_graph(self.ui.tab_8, "Electron mobility (cmˆ2 * Vˆ1 * sˆ-1)", self.electron_mobility)
+        self.draw_graph(self.ui.tab_9, "Hole mobility (cmˆ2 * Vˆ1 * sˆ-1)", self.Hole_mobility)
         self.draw_graph(self.ui.tab_2, "Hole concentration (1e18 cmˆ-3)", self.Hole_concentration / 1e18)
-        self.draw_graph(self.ui.tab_3, "Negative acceptor concentration (1e18 cmˆ-3)",
-                        self.Negative_acceptor_concentration / 1e18)
-        self.draw_graph(self.ui.tab_5, "Positive acceptor concentration (cmˆ-3)", self.Positive_acceptor_concentration)
+        if(self.flag_p):
+            self.draw_graph(self.ui.tab_3, "Positive acceptor concentration (cmˆ-3)", self.Positive_acceptor_concentration)
+        elif(self.flag_n):
+            self.draw_graph(self.ui.tab_3, "Negative acceptor concentration (1e18 cmˆ-3)",
+                            self.Negative_acceptor_concentration / 1e18)
         self.draw_graph(self.ui.tab_6, "Conductivity (Ωˆ-1 * cmˆ-1)", self.Conductivity)
         self.draw_graph(self.ui.tab_7, "Resistivity (Ω * cm)", self.Resistivity)
 
@@ -127,26 +151,20 @@ class MyWin(QtWidgets.QMainWindow):
     def onChanged_Temp(self, text):
         self.temperature = text
 
-    def onChanged_Ed(self, text):
-        self.Ed = text
+    def onChanged_E(self, text):
+        self.E = text
 
-    def onChanged_Ea(self, text):
-        self.Ea = text
+    def onChanged_Nmin(self, text):
+        self.Nmin = text
 
-    def onChanged_Namin(self, text):
-        self.Namin = text
+    def onChanged_Nmax(self, text):
+        self.Nmax = text
 
-    def onChanged_Namax(self, text):
-        self.Namax = text
 
     def onChanged_count(self, text):
         self.count = text
 
-    def onChanged_Ndmin(self, text):
-        self.Ndmin = text
 
-    def onChanged_Ndmax(self, text):
-        self.Ndmax = text
 
 
 if __name__ == "__main__":
